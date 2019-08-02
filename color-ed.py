8a206116b268e2a8de8edbc54c5b1898755d8eab
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-08-02 12:00:38 +0430

    reformat files and add data

--- color-ed.py
+++ color-ed.py
@@ -2,33 +2,32 @@ import PIL.Image as Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-def error_diffusion( pixel, size=(1,1) ):
-    for y in range(0, size[1]-1):
-        for x in range(1, size[0]-1):
+
+def error_diffusion(pixel, size=(1, 1)):
+    for y in range(0, size[1] - 1):
+        for x in range(1, size[0] - 1):
             oldpixel = pixel[x, y]
             if oldpixel > 127:
                 pixel[x, y] = 255
             else:
                 pixel[x, y] = 0
-                
-            quant_error = oldpixel - pixel[x,y]
-            pixel[x+1,y] = pixel[x+1,y] + (int)(7/16.0 * quant_error)
-            pixel[x-1, y+1] = pixel[x-1,y+1] + (int)(3/16.0 * quant_error)
-            pixel[x,y+1] = pixel[x,y+1] + (int)(5/16.0 * quant_error)
-            pixel[x+1,y+1] = pixel[x+1,y+1] + (int)(1/16.0 * quant_error)
+
+            quant_error = oldpixel - pixel[x, y]
+            pixel[x + 1, y] = pixel[x + 1, y] + int(7 / 16.0 * quant_error)
+            pixel[x - 1, y + 1] = pixel[x - 1, y + 1] + int(3 / 16.0 * quant_error)
+            pixel[x, y + 1] = pixel[x, y + 1] + int(5 / 16.0 * quant_error)
+            pixel[x + 1, y + 1] = pixel[x + 1, y + 1] + int(1 / 16.0 * quant_error)
     print(np.asarray(pixel))
-           
-             
-            
+
+
 if __name__ == "__main__":
-  img=Image.open('C:/Users/Hamed/Desktop/Halftoning/halftone algorithm/val_256/val_256/Places365_val_00004268.jpg')
-  im=img.convert('CMYK')
-  im = im.split()
-  dots = []
-  for chan in im:
-      error_diffusion( chan.load(), chan.size )
-  im = Image.merge("CMYK", im).convert("RGB")
-  imgplot = plt.imshow(im)
-  plt.show()
-  im.save('my_image.png')
-  
+    img = Image.open('C:/Users/Hamed/Desktop/Halftoning/halftone algorithm/val_256/val_256/Places365_val_00004268.jpg')
+    im = img.convert('CMYK')
+    im = im.split()
+    dots = []
+    for chan in im:
+        error_diffusion(chan.load(), chan.size)
+    im = Image.merge("CMYK", im).convert("RGB")
+    imgplot = plt.imshow(im)
+    plt.show()
+    im.save('my_image.png')
--- ed.py
+++ ed.py
@@ -2,49 +2,49 @@ import PIL.Image as Image
 import matplotlib.pyplot as plt
 
 
-def gen_matrix( e ):
-  ''' Generating new matrix.
-      @param e The width and height of the matrix is 2^e.
-      @return New 2x2 to 2^e x 2^e matrix list.
-  '''
-  if e < 1: return None
-  m_list = [ [[1,2],[3,0]] ]
-  _b = m_list[0]
-  for n in range(1, e):
-    m = m_list[ n - 1 ]
-    m_list.append( [
-      [4*i+_b[0][0] for i in m[0]] + [4*i+_b[0][1] for i in m[0]],
-      [4*i+_b[0][0] for i in m[1]] + [4*i+_b[0][1] for i in m[1]],
-      [4*i+_b[1][0] for i in m[0]] + [4*i+_b[1][1] for i in m[0]],
-      [4*i+_b[1][0] for i in m[1]] + [4*i+_b[1][1] for i in m[1]],
-    ] )
-  return m_list
+def gen_matrix(e):
+    """ Generating new matrix.
+    @param e The width and height of the matrix is 2^e.
+    @return New 2x2 to 2^e x 2^e matrix list.
+    """
+    if e < 1:
+      return None
+    m_list = [[[1, 2], [3, 0]]]
+    _b = m_list[0]
+    for n in range(1, e):
+        m = m_list[n - 1]
+        m_list.append([
+            [4 * i + _b[0][0] for i in m[0]] + [4 * i + _b[0][1] for i in m[0]],
+            [4 * i + _b[0][0] for i in m[1]] + [4 * i + _b[0][1] for i in m[1]],
+            [4 * i + _b[1][0] for i in m[0]] + [4 * i + _b[1][1] for i in m[0]],
+            [4 * i + _b[1][0] for i in m[1]] + [4 * i + _b[1][1] for i in m[1]],
+        ])
+    return m_list
 
 
-def ordered_dithering( pixel, size, matrix ):
-  """ Dithering on a single channel.
+def ordered_dithering(pixel, size, matrix):
+    """ Dithering on a single channel.
       @param pixel PIL PixelAccess object.
       @param size A tuple to represent the size of pixel.
       @param matrix Must be NxN, and N == 2^e where e>=1
-  """
-  X, Y = size
-  N = len(matrix)
-  
-  T = [[255*(matrix[x][y]+0.5)/N/N for x in range(N)] for y in range(N)]
-  for y in range(0, Y):
-    for x in range(0, X):
-      pixel[x,y] = 255 if pixel[x,y] > T[x%N][y%N] else 0
-      
-      
-      
+    """
+    X, Y = size
+    N = len(matrix)
+
+    T = [[255 * (matrix[x][y] + 0.5) / N / N for x in range(N)] for y in range(N)]
+    for y in range(0, Y):
+        for x in range(0, X):
+            pixel[x, y] = 255 if pixel[x, y] > T[x % N][y % N] else 0
+
+
 if __name__ == "__main__":
-  img=Image.open('C:/Users/Hamed/Desktop/Halftoning/halftone algorithm/val_256/val_256/Places365_val_00004289.jpg')
-  im=img.convert('CMYK')
-  im = im.split()
-  dots = []
-  #for chan in im:
-      #ordered_dithering( chan.load(), chan.size , gen_matrix(3)[3-1])
-  im = Image.merge("CMYK", im).convert("RGB")
-  imgplot = plt.imshow(im)
-  plt.show()
-  #im.save('my_image.png')
+    img = Image.open('C:/Users/Hamed/Desktop/Halftoning/halftone algorithm/val_256/val_256/Places365_val_00004289.jpg')
+    im = img.convert('CMYK')
+    im = im.split()
+    dots = []
+    # for chan in im:
+    # ordered_dithering( chan.load(), chan.size , gen_matrix(3)[3-1])
+    im = Image.merge("CMYK", im).convert("RGB")
+    imgplot = plt.imshow(im)
+    plt.show()
+    # im.save('my_image.png')
